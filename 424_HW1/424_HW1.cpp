b76ff56bf238f3f99021b0d89cea98e3840389d0
AspenStocking <a.stocking2002@gmail.com>
2024-02-01 11:38:25 -0500

    Add project files.

---
+++ 424_HW1/424_HW1.cpp
@@ -0,0 +1,31 @@
+// Homework 1, Question 1 
+// AERSP 424
+// Aspen Stocking
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	unsigned int AEW, passFront, passRear, useFuel, bagWeight;
+	
+	cout << "input aircraft empty weight (lbs): ";
+	cin >> AEW;
+	cout << endl << "input number of front passengers: ";
+	cin >> passFront;
+	cout << endl << "input number of rear passengers: ";
+	cin >> passRear;
+	cout << endl << "input ammount of useable fuel (gallons): ";
+	cin >> useFuel;
+	cout << endl << "input weight of baggage (lbs): ";
+	cin >> bagWeight;
+
+
+	
+	
+	
+	
+	
+	return 0;
+}
